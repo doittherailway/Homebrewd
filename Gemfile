b80f74b600a47d7dd9b54473b63f8a3568f1c299
railway <doittherailway@gmail.com>
2019-04-29 16:42:10 -0700

    Add users model, static_pages_controller and root views and routes

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -37,6 +37,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+
+    validates :username, :email, :password_digest, :session_token, uniqueness: true
+    validates :username, :email, :first_name, :last_name, :password_digest, :session_token, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password).is_password?(self.password_digest)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64(16)
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64(16)
+    end
+
+end
