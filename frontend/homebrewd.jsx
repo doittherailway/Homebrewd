fc01094bb634e8dde4803ff419f32e09b3b27e73
railway <doittherailway@gmail.com>
2019-04-29 17:01:38 -0700

    Initialize frontend setup including folder hierarchy, webpack and entry file

---
+++ frontend/homebrewd.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<h1>Welcome to Homebrewd</h1>, root);
+});
