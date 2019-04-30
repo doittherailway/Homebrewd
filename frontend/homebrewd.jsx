5085ad056d5cd8ae7afdba8aad87cc52c45847df
railway <doittherailway@gmail.com>
2019-04-30 11:38:37 -0700

    Add session_api_utils ajax requests and minor bug fixes to controllers

--- frontend/homebrewd.jsx
+++ frontend/homebrewd.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+
+    //Testing, remove
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
 
     ReactDOM.render(<h1>Welcome to Homebrewd</h1>, root);
 });
