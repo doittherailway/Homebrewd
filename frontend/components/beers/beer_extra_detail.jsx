e5855d06cf3b83404d85032158215de355368369
railway <doittherailway@gmail.com>
2019-05-06 22:35:03 -0700

    Add styling to beer component

--- frontend/components/beers/beer_extra_detail.jsx
+++ frontend/components/beers/beer_extra_detail.jsx
@@ -5,6 +5,8 @@ const BeerExtraDetail = (props) => {
     return(
         <div className="beer-extra-details">
             <p>{props.beer.abv}% ABV</p>
+            <p><i className="fas fa-certificate cert-ylw"></i><i className="fas fa-certificate cert-ylw"></i><i className="fas fa-certificate cert-ylw"></i><i className="fas fa-certificate cert-ylw"></i><i className="fas fa-certificate"></i> (4)</p>
+            <p> {Math.floor(Math.random() * 700) + 1} Ratings</p>
             <p>Added {props.beer.createdAt}</p>
         </div>
     );
