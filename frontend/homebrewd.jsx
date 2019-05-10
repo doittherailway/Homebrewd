0e99cc0bf4bf27b9c43657750444b193cdf32c94
railway <doittherailway@gmail.com>
2019-05-10 10:05:43 -0700

    Remove testing functions

--- frontend/homebrewd.jsx
+++ frontend/homebrewd.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchCheckins, fetchResourceCheckins, createCheckin } from './actions/checkin_actions';
-import { searchBeers } from './util/search_api_util';
-import { fetchResults } from './actions/search_actions';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,12 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
     else {
         store = configureStore();
     }
-
-    //testing remove later
-    window.dispatch = store.dispatch;
-    window.getState = store.getState;
-    window.searchBeers = searchBeers;
-    window.fetchResults = fetchResults;
 
 
     ReactDOM.render(<Root store={store} />, root);
